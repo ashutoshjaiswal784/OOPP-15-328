4feaea7c6387db18d210cbd728e3a4b6edb47f93
Ashutosh kumar jaiswal <ashutoshjaiswal784@gmail.com>
2026-09-19 10:58:59 +0530

    New

---
+++ lab6/Experiment6.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+private:
+    int rollNo;
+    string name;
+
+public:
+    Student()
+    {
+        rollNo = 0;
+        name = "Unknown";
+
+        cout << "Default Constructor called" << endl;
+    }
+    Student(int r, string n)
+    {
+        rollNo = r;
+        name = n;
+
+        cout << "Parameterized Constructor called" << endl;
+    }
+    Student(const Student &s)
+    {
+        rollNo = s.rollNo;
+        name = s.name;
+
+        cout << "Copy Constructor called" << endl;
+    }
+    void display()
+    {
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+    }
+    ~Student()
+    {
+        cout << "Destructor called for " << name << endl;
+    }
+};
+
+int main()
+{
+    cout << "Creating first object:" << endl;
+    Student s1;
+    s1.display();
+
+    cout << "Creating second object:" << endl;
+    Student s2(101, "Ashutosh");
+    s2.display();
+
+    cout << "Creating third object using copy constructor:" << endl;
+    Student s3(s2);
+    s3.display();
+
+    cout << "End of main()" << endl;
+
+    return 0;
+}
